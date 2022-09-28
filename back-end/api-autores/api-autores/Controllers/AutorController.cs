cbcad0c1fcd1634de6adc85e9809c5ad589a6c49
Matias2767 <matiasrodriguezpozo14@gmail.com>
2022-09-28 15:09:59 -0500

    segunda subida

---
+++ back-end/api-autores/api-autores/Controllers/AutorController.cs
@@ -0,0 +1,41 @@
+﻿using api_autores.Entitys;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace api_autores.Controllers
+{
+    //dinciamos que es un controlador
+    [ApiController]
+    //es definir la ruta de acceso al controlador
+    [Route("api-autores/autor")]
+    //: ControllerBase es una herencia para que sea un controlador
+    public class AutorController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        //creamos el método constructor
+        public AutorController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+
+        //cuando queremos obtener informacion
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> findAll()
+        {
+            return await context.Autor.ToListAsync();
+        }
+
+        //cuando queremos guardar información
+        [HttpPost]
+        public async Task<ActionResult> add(Autor a)
+        {
+            context.Add(a);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ back-end/api-autores/api-autores/Entitys/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api_autores.Entitys
+{
+    public class Autor
+    {
+        //definimos la clave primaria
+        [Key]
+        public int codigo { get; set; }
+        //definimos valores no nulos
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(
+            maximumLength:100, 
+            ErrorMessage ="Se tiene que ingresar un nombre"
+            )]
+        public string nombre { get; set; }  
+        [Required]
+        public bool estado { get; set; }
+    }
+}
